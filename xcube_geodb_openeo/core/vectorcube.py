a9b4448a9550a10d00dc65e9c9c9025626d00594
thomas <thomas.storm@brockmann-consult.de>
2023-06-22 17:07:56 +0200

    added some thoughts about vector cubes

--- xcube_geodb_openeo/core/vectorcube.py
+++ xcube_geodb_openeo/core/vectorcube.py
@@ -18,10 +18,84 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-
+from datetime import datetime
 from typing import Any
 from typing import Dict
+from typing import List
+from geojson.geometry import Geometry
+
 
 VectorCube = Dict[str, Any]
 
 Feature = Dict[str, Any]
+
+class VectorCube:
+
+    """
+    Representation of a VectorCube, according to
+    https://r-spatial.org/r/2022/09/12/vdc.html.
+
+    When is a geoDB table column a dimension?
+    In a vector cube, the geometry always is a dimensions. If the geometry in
+    the geoDB is 3-dimensional (is that possible anyway?), we have to split
+    that into a 2d geometry and an additional vertical dimension.
+    The time column translates to a dimension as well.
+    Other columns (say, chl_mean) never translate to a dimension; rather, each
+    position in the cube (given by geometry, maybe z, and time) contains an
+    array of unspecific type. Thus, we do not have additional dimensions for
+    geoDB tables.
+    It could be useful to make a column a dimension, if the column changes
+    its value only if all combinations of values of the other dimensions
+    change. Wavelength is a good example: for 440nm, there are entries for all
+    geometries and datetimes, and for 550nm, there are others.
+    However, detecting this from a table is hard.
+    """
+
+    @property
+    def vector_dim(self) -> List[Geometry]:
+        """
+        Represents the vector dimension of the vector cube. By definition,
+        vector data cubes have (at least) a single spatial dimension.
+
+        What is a geometry dim?
+            Explicitly defined: a list of all the different values, e.g.  [POINT(5 7), POINT(1.3 4), POINT(8 3)]
+            Implicitly defined: simply a name, which can be used to get the values from a dict or something
+            -> we define it explicitly
+        # shall we support multiple geometry dimensions?
+            - check openEO requirements/specifications on this
+        # shall we return GeoJSON? Or CovJSON?
+            - use what's best suited for internal use
+            - go with GeoJSON first
+        # shall support lazy loading: read values and dimensions only when asked for, i.e. when this method is called
+
+        :return: list of geojson geometries
+        """
+        pass
+
+    @property
+    def vertical_dim(self) -> List[Any]:
+        """
+        Represents the vertical geometry dimension of the vector cube, if it
+        exists. Returns the explicit list of dimension values. If the vector
+        cube does not have a vertical dimension, returns an empty list.
+        :return: list of dimension values, typically a list of float values.
+        """
+        pass
+
+    @property
+    def time_dim(self) -> List[datetime]:
+        """
+        Returns the time dimension of the vector cube as an explicit list of
+        datetime objects. If the vector cube does not have a time dimension,
+        an empty list is returned.
+        """
+        pass
+
+    @property
+    def additional_dims(self) -> List[Any]:
+        """
+        Returns all dimensions of the vector cube, which are neither the
+        vector dimension, nor the vertical dimension, nor the time dimension.
+        For example: colors with values R,G,B is a dimension.
+        """
+        pass
